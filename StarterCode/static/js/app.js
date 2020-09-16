ebbb601a65b69310844bea6aeda6c31daad0cf58
egipe <gipe.elisa@gmail.com>
2020-09-15 21:34:13 -0700

    adding instructions

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,11 @@
+ // Drop Down menu to display top 10 OTU's
+ 
+
+// Horizontal Bar Chart- sample_values as values for bar chart
+// otu_ids as labels
+// otu_labels as hovertext
+
+// Bubble chart- otu's x values, sample_values for y & marker size, otu_id for marker color, otu_labels for text values
+//Display sample metadata (individuals demographic info)
+
+// Display each key-value pair from metadata JSON object
