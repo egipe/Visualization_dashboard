162e581c060c838f012397e7211d278c4d1f28fc
egipe <gipe.elisa@gmail.com>
2020-09-18 21:01:08 -0700

    finally showing some charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -19,64 +19,76 @@ function create_dropdown(){
 
 create_dropdown()
 
-// function optionChanged(new_selection){
-    // 3.json("samples.json").then(function(data){
-        // var samples = data.samples
-        // samples.forEach(sample =>{
-            // some kind of if statement if id matches
-        // })
-    // })
-//}
-
-// Horizontal Bar Chart- sample_values as values for bar chart, otu_ids as labels, otu_labels as hovertext; top 10 OTU's
-function getData() {
-    d3.json("samples.json").then(function(data){
-        var id
-        var ethnicity
-        var gender
-        var location
-        var bbtype
-        var wfreq
-        var otu_ids = data.samples.otu_ids;
-        var sample_values = data.samples.sample_values;
-        var otu_labels = data.samples.otu_labels;
-        buildTable(id, ethnicity, gender, location, bbtype, wfreq);
-    });
+function optionChanged(new_selection){
+     buildMetaData(new_selection)
+     buildPlot(new_selection)
+     buildBubble(new_selection)
 }
 
-// Display each key-value pair from metadata JSON object
-function buildTable(id, ethnicity, gender, location, bbtype, wfreq) {
-    var table = d3.select(".sample-metadata")
+// // Display each key-value pair from metadata JSON object
+function buildMetaData(selection) {
+    d3.json("samples.json").then(function(data){
+        var samples = data.metadata;
+        // other charts will call data.sample
+        //filter by whatever is chosen
+        var new_array = samples.filter(sample => sample.id == selection);
+        console.log(new_array);
+        var div_tag = d3.select("#sample-metadata")
+        div_tag.html("")
+        Object.entries(new_array[0]).forEach(([key, value]) => div_tag.append("h3").text(`${key}: ${value}`));
+     });
 }
 
-function buildPlot() {
+// // Horizontal Bar Chart- sample_values as values for bar chart, otu_ids as labels, otu_labels as hovertext; top 10 OTU's
+function buildPlot(selection) {
     d3.json("samples.json").then(function(data){
-        getData()
-
+        var samples = data.samples;
+        var new_array = samples.filter(sample => sample.id == selection);
         var trace1 = {
             type: "bar",
-            x: otu_ids.slice(0,9)
-            //slice for top 10
-            y: sample_values
+            //slice for top 10 and order
+            x: new_array[0].otu_ids.slice(0,10).reverse(),
+            y: new_array[0].sample_values.slice(0,10).reverse(),
+            orientation: "h"
         }
-        var data = [trace1]
+        var data = [trace1];
         var layout = {
-            title: `top 10 OTUs`
+            title: `top 10 OTUs`,
             xaxis: {
-            }
+                title: "OTU IDs"
+            },
             yaxis: {
+                title: "Sample Values"
             }
             };
         Plotly.newPlot('bar', data, layout)
     });
 }
+optionChanged("940");
 
-buildPlot();
-
-// Bubble chart- otu's x values, sample_values for y & marker size, otu_id for marker color, otu_labels for text values
-//Display sample metadata (individuals demographic info)
-
-
-// Update charts when new subject is selected 
-
-plotly.restyle('bar')
+// // Bubble chart- otu's x values, sample_values for y & marker size, otu_id for marker color, otu_labels for text values
+function buildBubble(selection) {
+    d3.json("samples.json").then(function(data){
+        var samples = data.samples;
+        var new_array = samples.filter(sample => sample.id == selection);
+        var trace1 = {
+            type: "bubble",
+            //slice for top 10 and order
+            x: new_array[0].otu_ids,
+            y: new_array[0].sample_values,
+            mode: "markers",
+            marker: new_array[0].sample_values
+        }
+        var data = [trace1];
+        var layout = {
+            title: `top 10 OTUs`,
+            xaxis: {
+                title: "OTU IDs"
+            },
+            yaxis: {
+                title: "Sample Values"
+            }
+            };
+        Plotly.newPlot('bubble', data, layout)
+    });
+}
